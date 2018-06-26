2730f3e3d3a94824980f7bf2b94bb1b420d489b7
took13 <took13@hotmail.com>
2018-06-26 17:58:29 +0700

    Update config.php

--- config.php
+++ config.php
@@ -7,10 +7,9 @@ return (object) array(
     'db_username' => $url["user"],
     'db_pass' => $url["pass"],
     'db_database' => substr($url["path"], 1),
-    'channelID' => '*******',
-    'channelAccessToken' => '*******',
-    'channelSecret' => '*******',
-
+    'channelID' => '1493623889',
+    'channelAccessToken' => 'XCBJd9Dqc1kfB/VLFnVuEWJ/AjkLXgLdLnSbGuqXeMDwvpfV/XF3iHVtZNso7Jq4onCK9fmbHQ8MZsgrU7ZjCS1+tQk+STz2/0gm4Juod3sG7lRtRmv6bRGtGPAZABQqH3593dxp7W+ladz/KDMR0gdB04t89/1O/w1cDnyilFU=',
+    'channelSecret' => 'd831e7e4bd15e966b258ed6757a9cb99'
 );
 
 ?>
